ddcb06f31bde8bb31f41d4d81f9433e4e8c9cfad
mohammedshoqi123-art <mohammedshoqi123-art@users.noreply.github.com>
2026-04-21 16:49:06 +0000

    deploy: Flutter Web update [skip ci] 4887a0c9de8da779d644db1812bbb882f9a3d480

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -18,7 +18,7 @@ const RESOURCES = {"assets/assets/fonts/Tajawal-Medium.ttf": "3876865bd2f341d2d9
 "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
-"assets/fonts/MaterialIcons-Regular.otf": "ca5de3b9da473319c82b2e8e50c8be54",
+"assets/fonts/MaterialIcons-Regular.otf": "859b633d7218d13259a886c98cf71a14",
 "assets/AssetManifest.bin": "5e19a68797170ab610630aeda472116b",
 "assets/NOTICES": "0369190bc3fcfbddbf7a1057e72b587e",
 "assets/FontManifest.json": "d12b6d39f93e611f47414074f85ca433",
@@ -36,8 +36,8 @@ const RESOURCES = {"assets/assets/fonts/Tajawal-Medium.ttf": "3876865bd2f341d2d9
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "9177abb90af8526930f7e0e5a0505ed5",
-"main.dart.js": "8b72e539fb59c9e9d60428b46c2da1f0",
+"flutter_bootstrap.js": "984d2c422097b468f6a213c9f6978d31",
+"main.dart.js": "e9ee9a1ba0aeb159fd9c32e822d881bc",
 "version.json": "f5e1e04d4e61f48538135d4b1a4947f0"};
 // The application shell files that are downloaded before a service worker can
 // start.

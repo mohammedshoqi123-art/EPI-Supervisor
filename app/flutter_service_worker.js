f3b746bc072371b357e808529a1bac0bb2e31915
mohammedshoqi123-art <mohammedshoqi123-art@users.noreply.github.com>
2026-04-22 18:36:14 +0000

    deploy: Flutter Web update [skip ci] 111cdaa55b7d2553c056f23befaad9fa3bca29ee

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -36,7 +36,7 @@ const RESOURCES = {"assets/assets/fonts/Tajawal-Medium.ttf": "3876865bd2f341d2d9
 "canvaskit/canvaskit.js": "26eef3024dbc64886b7f48e1b6fb05cf",
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
-"flutter_bootstrap.js": "2539bc668cac0ba76a2f53cf45a04d59",
+"flutter_bootstrap.js": "cd27b4e4433157170ad5a3476a3be3d6",
 "main.dart.js": "a0846ea3eb40af807a0a470c8d348fba",
 "version.json": "f5e1e04d4e61f48538135d4b1a4947f0"};
 // The application shell files that are downloaded before a service worker can

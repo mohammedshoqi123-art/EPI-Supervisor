94cfa76646cde3d7c4699c9912e8115e157d1b1c
mohammedshoqi123-art <mohammedshoqi123-art@users.noreply.github.com>
2026-07-11 22:02:11 +0000

    deploy: Flutter Web update [skip ci] 79618b27ee88b1eda2b86bc7fd7cf4d2d6ec102d

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -23,7 +23,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "assets/AssetManifest.bin": "5e19a68797170ab610630aeda472116b",
 "assets/NOTICES": "4c01b9bb2a00e94fa56a8588b428d490",
 "assets/AssetManifest.json": "b5235826431e8e5008c28a430aeca571",
-"flutter_bootstrap.js": "82405090684987d8266a94791329cb0b",
+"flutter_bootstrap.js": "5697790a8fa495db9da9e3cb42096a4a",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/skwasm.js.symbols": "96263e00e3c9bd9cd878ead867c04f3c",
 "canvaskit/chromium/canvaskit.js": "b7ba6d908089f706772b2007c37e6da4",
@@ -38,7 +38,7 @@ const RESOURCES = {"flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "/": "c1e4f982ae563a28d55c26b94f5f36ba",
 "main.dart.js": "985f8048ff445d69129794f6fc73456b",
 "manifest.json": "15ef2bcd9c1590ae9229a0e16e1559de",
-"version.json": "a57e8b0383587d1a96ca8acf051bef3c"};
+"version.json": "ef1ec6763227daa3e1f161d1f32ad10b"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",

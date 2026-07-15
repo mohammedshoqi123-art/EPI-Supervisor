d406aa10288488b1d43e962c8be06fa77a1a00e0
EPI Developer <developer@epi-supervisor.com>
2026-07-15 20:42:04 +0800

    fix: AuthState name conflict in profile_screen.dart
    Added 'hide AuthState' to supabase_flutter import to resolve
    name conflict with epi_core's AuthState class.

--- apps/mobile/lib/screens/profile_screen.dart
+++ apps/mobile/lib/screens/profile_screen.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart' hide AuthState;
 import 'package:epi_shared/epi_shared.dart';
 import 'package:epi_core/epi_core.dart';
 import '../main.dart';

7d3e52be1eb00ce96e73c9078b6b1910f0f6f163
mohammedshoqi123-art <mohammedshoqi123-art@users.noreply.github.com>
2026-07-18 22:43:45 +0000

    deploy: Flutter Web update [skip ci] b6411979bc9f118218bb4fb1e6cda652f705fe58

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -6,7 +6,7 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {"index.html": "c1e4f982ae563a28d55c26b94f5f36ba",
 "/": "c1e4f982ae563a28d55c26b94f5f36ba",
 "version.json": "69f898c893662d07347d00c9642568d4",
-"main.dart.js": "b8fb7000b494a883b4575d7904404f3a",
+"main.dart.js": "6340584d7a80dd390bca776ee7cbded4",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
@@ -38,7 +38,7 @@ const RESOURCES = {"index.html": "c1e4f982ae563a28d55c26b94f5f36ba",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "manifest.json": "15ef2bcd9c1590ae9229a0e16e1559de",
-"flutter_bootstrap.js": "30311d53181a553c000cbb604830e161"};
+"flutter_bootstrap.js": "85fb3337e09ba3bb48067f7b8b00e843"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",

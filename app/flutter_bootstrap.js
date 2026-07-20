cb61423ec0ae01672cb3ef828baa781ed42a8d9c
mohammedshoqi123-art <mohammedshoqi123-art@users.noreply.github.com>
2026-07-20 04:25:53 +0000

    deploy: Flutter Web update [skip ci] c23ae882e256cedd962b81c45a76d46e8702011d

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "861719779"
+    serviceWorkerVersion: "1134280379"
   }
 });

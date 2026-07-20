9515b35fa8604a3fa60a58d22a2b81beeb95b60b
mohammedshoqi123-art <mohammedshoqi123-art@users.noreply.github.com>
2026-07-20 05:45:24 +0000

    deploy: Flutter Web update [skip ci] cecf7a442d811875a60fe21832beea97dc88fac4

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -38,7 +38,7 @@ const RESOURCES = {"index.html": "c1e4f982ae563a28d55c26b94f5f36ba",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/canvaskit.js.symbols": "efc2cd87d1ff6c586b7d4c7083063a40",
 "manifest.json": "15ef2bcd9c1590ae9229a0e16e1559de",
-"flutter_bootstrap.js": "39cffeb8941011eceb7a3461317cf912"};
+"flutter_bootstrap.js": "0957b8eb4a2197fa452d116f4f4597b6"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",

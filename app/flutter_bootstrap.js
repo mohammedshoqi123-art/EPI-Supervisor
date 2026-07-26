af0df875e94c36e1ddebd0bffea3493a2e020dff
mohammedshoqi123-art <mohammedshoqi123-art@users.noreply.github.com>
2026-07-26 02:44:16 +0000

    deploy: Flutter Web update [skip ci] b26b4efb757de72582cf76f7bd68926386f59adf

--- app/flutter_bootstrap.js
+++ app/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4218881012"
+    serviceWorkerVersion: "548212417"
   }
 });
--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "374bbe4936d1805f568bcd4d534729ee",
+const RESOURCES = {"flutter_bootstrap.js": "b08e60bf95cf60d72466d59e5c6b776c",
 "index.html": "c1e4f982ae563a28d55c26b94f5f36ba",
 "/": "c1e4f982ae563a28d55c26b94f5f36ba",
-"main.dart.js": "429056c5f92276832ed9e4ad8f71f533",
+"main.dart.js": "c1ffd940a3e54efab4a301f0f28c7d46",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",

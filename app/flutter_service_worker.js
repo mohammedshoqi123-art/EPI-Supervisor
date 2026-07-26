5f87729c9ba811d77b9c34c68a10a7cd3acea765
mohammedshoqi123-art <mohammedshoqi123-art@users.noreply.github.com>
2026-07-26 02:55:24 +0000

    deploy: Flutter Web update [skip ci] e6a4fb0142ccf104a045e5589b419f2d722f0c1a

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "b08e60bf95cf60d72466d59e5c6b776c",
+const RESOURCES = {"flutter_bootstrap.js": "cb66d363fb5c66cea5122285df9a5267",
 "index.html": "c1e4f982ae563a28d55c26b94f5f36ba",
 "/": "c1e4f982ae563a28d55c26b94f5f36ba",
-"main.dart.js": "c1ffd940a3e54efab4a301f0f28c7d46",
+"main.dart.js": "295a505591015f2fe35d8ff97f117acb",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",

cbe926d3210bb2fb4b5558b973d6d4fec0df0855
mohammedshoqi123-art <mohammedshoqi123-art@users.noreply.github.com>
2026-08-02 20:41:13 +0000

    deploy: Flutter Web update [skip ci] bf47ce25e9501678538bf716fd69b9680e29270e

--- app/flutter_service_worker.js
+++ app/flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d06dfeb2b1a3f4f3a39164498c326eda",
+const RESOURCES = {"flutter_bootstrap.js": "6184e2c17802de37cafda715fd45f9fc",
 "index.html": "c1e4f982ae563a28d55c26b94f5f36ba",
 "/": "c1e4f982ae563a28d55c26b94f5f36ba",
-"main.dart.js": "d03d497cca61452c0191a3402fdd0e46",
+"main.dart.js": "c61af73371df3dc0372353cd76d59c91",
 "flutter.js": "4b2350e14c6650ba82871f60906437ea",
 "canvaskit/skwasm.wasm": "828c26a0b1cc8eb1adacbdd0c5e8bcfa",
 "canvaskit/skwasm.js": "ac0f73826b925320a1e9b0d3fd7da61c",
@@ -24,7 +24,7 @@ const RESOURCES = {"flutter_bootstrap.js": "d06dfeb2b1a3f4f3a39164498c326eda",
 "assets/AssetManifest.bin": "a99675c14072a51ab64631f8dcba5493",
 "assets/NOTICES": "4c01b9bb2a00e94fa56a8588b428d490",
 "assets/AssetManifest.json": "4265eda55a002826b92f02243b8a4107",
-"assets/fonts/MaterialIcons-Regular.otf": "f20ee203aa50dc8728e75e2f6318e9fd",
+"assets/fonts/MaterialIcons-Regular.otf": "cbf723af535746ff29f981f49915c523",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "e986ebe42ef785b27164c36a9abc7818",
 "assets/packages/flutter_map/lib/assets/flutter_map_logo.png": "208d63cc917af9713fc9572bd5c09362",
 "assets/FontManifest.json": "d12b6d39f93e611f47414074f85ca433",
